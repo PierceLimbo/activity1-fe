39c6ba73a6740e71fbe01127cd833fa77e873efc
Pierce Limbo <plimbo@unomaha.edu>
2025-02-11 09:33:50 -0600

    d

--- src/http/APIService.js
+++ src/http/APIService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // Change the API_URL to the correct location of the backend API before deploying the app
-const API_URL = 'http://plimbo.pythonanywhere.com/'; /* 'http://localhost:8000' http://127.0.0.1:8000/ or  'http://127.0.0.1:8000/'*/
+const API_URL = 'https://plimbo.pythonanywhere.com'; /* 'http://localhost:8000' http://127.0.0.1:8000/ or  'http://127.0.0.1:8000/'*/
 
 export class APIService {
   constructor() {
